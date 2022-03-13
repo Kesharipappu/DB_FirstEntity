85cc6f83f9f415b66360b860880e1a0e903e7580
PAPPU KUMAR KESHARI <92083274+Kesharipappu@users.noreply.github.com>
2022-03-12 21:44:28 -0800

    Add project files.

---
+++ DB_FirstEntity/Models/EmployeeDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DB_FirstEntity.Models
+{
+    //DTO->Data Transfer Object
+    public class EmployeeDetails
+    {
+        //employee POCO->Plain Old CLR Object
+        public int EId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        //Address POCO->Plain Old CLR Object
+        public string AddressLine { get; set; } 
+        public string State { get; set; }
+        public string City { get; set; }
+
+    }
+}
